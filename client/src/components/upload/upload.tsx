ec5b089cd14bd0061bba20bc7bf5ac94900ff8d9
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2018-12-24 11:53:47 +0100

    Input upload field.

--- client/src/components/upload/upload.tsx
+++ client/src/components/upload/upload.tsx
@@ -3,9 +3,26 @@ import {ReactNode} from "react";
 
 export default class Upload extends React.Component {
 
+    private _fileField: any;
+    private _previewField: any;
+    private fileReader = new FileReader();
+
+    constructor() {
+        super({});
+        this.onFileChosen = this.onFileChosen.bind(this);
+    }
+
+    private onFileChosen(event: any): void {
+        this.fileReader.readAsDataURL(this._fileField.files[0]);
+        this.fileReader.onload = (ev) => {
+            this._previewField.src = (ev.target as FileReader).result;
+        };
+    }
+
     render(): ReactNode {
-        return <article>
-            <h1>Upload</h1>
+        return <article className="upload">
+            <input type="file" accept="image/*" capture ref={(f) => {this._fileField = f; }} onChange={this.onFileChosen} />
+            <img ref={(p) => {this._previewField = p; }} alt="Preview" />
         </article>;
     }
 
