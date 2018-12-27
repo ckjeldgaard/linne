b3b306d9f5fc05eb44f25a9c50c0ea15139f2e36
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2018-12-27 12:47:35 +0100

    Refactored ImageUpload class to use async/await.

--- client/src/domain/image-upload.ts
+++ client/src/domain/image-upload.ts
@@ -5,7 +5,6 @@ import * as config from "react-global-configuration";
 import * as firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
-import UploadTaskSnapshot = firebase.storage.UploadTaskSnapshot;
 import {ResizePhoto} from "./photo/resize-photo";
 import {ContrastPhoto} from "./photo/contrast-photo";
 import {BrightenPhoto} from "./photo/brighten-photo";
@@ -19,6 +18,10 @@ export class ImageUpload {
 
     private readonly firebaseApp: firebase.app.App;
 
+    private static readonly BRIGHTNESS: number = 50;
+    private static readonly CONTRAST: number = 90;
+    private static readonly IMAGE_SIZE_PIXELS: number = 28;
+
     constructor(
         private readonly blob: Blob,
         private readonly originalImageWidth: number,
@@ -29,66 +32,55 @@ export class ImageUpload {
         this.firebaseApp = firebase.initializeApp(config.get("firebaseConfig"));
     }
 
-    async upload(): Promise<void> {
+    async upload(): Promise<void[]> {
         let photo = new ResizePhoto(
             new ContrastPhoto(
                 new BrightenPhoto(
                     new CroppedPhoto(this.blob, this.originalImageWidth, this.originalImageHeight),
-                    50),
-                90),
-            28,
-            28
+                    ImageUpload.BRIGHTNESS),
+                ImageUpload.CONTRAST),
+            ImageUpload.IMAGE_SIZE_PIXELS,
+            ImageUpload.IMAGE_SIZE_PIXELS
         );
 
         const imageData = await photo.draw();
 
-        this.uploadOriginal(async (snapshot: UploadTaskSnapshot, downloadUrl: string) => {
+        const snapshot = await this.uploadOriginal();
+        const downloadUrl = await ImageUpload.getImageDownloadUrl(snapshot.ref);
 
-            const coin = new ClassifiedCoin(snapshot.metadata.name, downloadUrl, this.chosenCoin.id, this.chosenSide, 0, imageData);
-            console.log("coin = ", coin.toObject());
-            this.writeCoinToDatabase(coin);
+        const coin = new ClassifiedCoin(snapshot.metadata.name, downloadUrl, this.chosenCoin.id, this.chosenSide, 0, imageData);
+        console.log("coin = ", coin.toObject());
 
-            // Upload 3 additional versions of the image rotated 90 degrees each:
-            for (let i = 90; i < 360; i += 90) {
-                const rotatedImage = await new RotatePhoto(photo, i).draw();
-                const coin = new ClassifiedCoin(snapshot.metadata.name, downloadUrl, this.chosenCoin.id, this.chosenSide, i, rotatedImage);
-                this.writeCoinToDatabase(coin);
-            }
+        const coinRequests = [];
+        coinRequests.push(this.writeCoinToDatabase(coin));
 
-            console.log("Successfully uploaded!", snapshot);
-        });
+        // Upload 3 additional versions of the image rotated 90 degrees each:
+        for (let i = 90; i < 360; i += 90) {
+            const rotatedImage = await new RotatePhoto(photo, i).draw();
+            const coin = new ClassifiedCoin(snapshot.metadata.name, downloadUrl, this.chosenCoin.id, this.chosenSide, i, rotatedImage);
+            coinRequests.push(this.writeCoinToDatabase(coin));
+        }
+
+        return await Promise.all(coinRequests);
     }
 
-    private writeCoinToDatabase(classifiedImage: ClassifiedCoin): void {
+    private async writeCoinToDatabase(classifiedImage: ClassifiedCoin): Promise<void> {
         const db: firebase.firestore.Firestore = this.firebaseApp.firestore();
         const key: string = db.collection("coins").doc().id;
-
-        db.collection("coins").doc(key)
-            .set(classifiedImage.toObject())
-            .then(() => {
-                console.log("Coin successfully written!");
-            })
-            .catch((error) => {
-                console.error("Error writing document: ", error);
-
-            });
+        return await db.collection("coins").doc(key).set(classifiedImage.toObject());
     }
 
-    private uploadOriginal(onFulfilled: ((snapshot: firebase.storage.UploadTaskSnapshot, downloadUrl: string) => any)): void {
+    private async uploadOriginal(): Promise<firebase.storage.UploadTaskSnapshot> {
         const fileName = uuidv1() + ".jpg";
         const file: File = new ImageFileConverter(this.blob).toFile(fileName);
 
         const storageRef = this.firebaseApp.storage().ref();
         const originalsRef = storageRef.child("originals/" + fileName);
 
-        originalsRef.put(file).then((snapshot: UploadTaskSnapshot) => {
-            snapshot.ref.getDownloadURL().then((downloadURL: string) => {
-                onFulfilled(snapshot, downloadURL);
-            });
+        return await originalsRef.put(file);
+    }
 
-        }).catch(reason => {
-            console.error("Upload failed", reason);
-        });
-
+    private static async getImageDownloadUrl(ref: firebase.storage.Reference): Promise<string> {
+        return await ref.getDownloadURL();
     }
 }
