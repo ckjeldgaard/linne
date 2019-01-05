89c4df5bc858bcbc80d75937ec19eb194e8690b1
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2019-01-05 23:24:47 +0100

    Created first cloud function fetching data from Firestore.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from "firebase-functions";
+import * as firebase from "firebase-admin";
+import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firestore Database.
+firebase.initializeApp();
+
+const getTrainingImages = async function(): Promise<void> {
+
+    const querySnapshot: firebase.firestore.QuerySnapshot = await firebase.firestore().collection("training").limit(2).get();
+    querySnapshot.forEach((doc: DocumentSnapshot) => {
+        console.log("Got doc with ID = ", doc.id);
+    });
+
+};
+
+export const tensorflow = functions.https.onRequest(async (request, response) => {
+    await getTrainingImages();
+
+    response.send("Hello from Firebase/tensorflow!");
+});
+
