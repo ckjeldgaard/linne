ef9838ebe3e253f120ffbea22133f08d435f0641
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2019-01-05 16:23:07 +0100

    Added Scaffolding component to upload training data.

--- client/src/components/content-area/content-area.tsx
+++ client/src/components/content-area/content-area.tsx
@@ -3,6 +3,7 @@ import {ReactNode} from "react";
 import {Route, Switch} from "react-router";
 import Detect from "../detect/detect";
 import Upload from "../upload/upload";
+import Scaffolding from "../scaffolding/scaffolding";
 
 export default class ContentArea extends React.Component {
 
@@ -10,6 +11,7 @@ export default class ContentArea extends React.Component {
         return <Switch>
             <Route exact path="/" component={Detect} />
             <Route exact path="/upload" component={Upload} />
+            <Route exact path="/stats" component={Scaffolding} />
         </Switch>;
     }
 
